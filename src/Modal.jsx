5906ce1aac751c70edc93a5fc1c30b9db9d54fd1
Mirko Zlatunic <m.zlatunic@gmail.com>
2025-03-01 17:04:54 -0800

    mobile version completed

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import image from './assets/images/image-product-desktop.jpg';
+import { useState, useEffect } from 'react';
+import imgDesktop from './assets/images/image-product-desktop.jpg';
+import imgMobile from './assets/images/image-product-mobile.jpg';
 import cart from './assets/images/icon-cart.svg';
+
 function Modal() {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 768);
+    };
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="grid grid-cols-2 bg-white max-w-xl rounded-lg">
-      <img
-        src={image}
-        alt="desktop pic"
-        className="rounded-tl-lg rounded-bl-lg h-full"
-      />
+    <div className="md:grid md:grid-cols-2 bg-white md:max-w-xl max-w-[350px] rounded-lg">
+      {isDesktop ? (
+        <img
+          src={imgDesktop}
+          alt="desktop pic"
+          className="rounded-tl-lg rounded-bl-lg h-full"
+        />
+      ) : (
+        <img src={imgMobile} alt="mobile pic" className="rounded-t-lg h-full" />
+      )}
+
       <div className="p-8">
         <div className="pb-6">
           <p className="text-[#6C7289] uppercase tracking-[0.5em] font-montserrat text-xs pb-4">
@@ -28,7 +51,7 @@ function Modal() {
           </p>
           <p className="font-montserrat line-through">$169.99</p>
         </div>
-        <div className="flex items-center justify-center bg-[#3d8168] py-4 rounded-lg cursor-pointer">
+        <div className="flex items-center justify-center bg-[#3d8168] hover:bg-[#1A4032] py-4 rounded-lg cursor-pointer">
           <img src={cart} alt="cart" />
           <button className="pl-2 font-montserrat text-sm text-white cursor-pointer">
             Add to Cart
